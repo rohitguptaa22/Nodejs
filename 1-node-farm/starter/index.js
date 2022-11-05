67631470cdd8acf5d8b8f633c5152f9c78264a0c
rohitGupta2022 <104063767+rohitGupta2022@users.noreply.github.com>
2022-11-05 17:32:44 +0530

    date.now() issue resolved

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
 // console.log(textIn);
 
-const textOut = ` This is what we know about the avocado: ${textIn}.\Created on ${Date.now}`
+const textOut = ` This is what we know about the avocado: ${textIn}.\Created on ${Date.now()}`
 fs.writeFileSync('./txt/output.txt', textOut)
 console.log(textOut);
