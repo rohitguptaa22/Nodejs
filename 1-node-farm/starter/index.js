675e289bfa094393a0d108f16daa9cfcc9fe3b34
rohitGupta2022 <104063767+rohitGupta2022@users.noreply.github.com>
2022-11-07 09:25:33 +0530

    sync vs async

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,29 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// // Blocking, sync method.
+// const textIn = fs.readFileSync('./txt/start.txt', 'utf-8')
 // console.log(textIn);
 
-const textOut = ` This is what we know about the avocado: ${textIn}.\Created on ${Date.now()}`
-fs.writeFileSync('./txt/output.txt', textOut)
-console.log(textOut);
+// const textOut = ` This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log(textOut);
+
+//NO BLOCKING ASYNCHRONOUS WAY:
+
+fs.readFile('./txt/starth.txt', 'utf-8', (err, data1) => {
+    if (err) {
+        console.log("ERROR");
+        return
+    }
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3);
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+
+                console.log("Your file has been written!!");
+            })
+        })
+    })
+})
+console.log('Will Read FIle..')
