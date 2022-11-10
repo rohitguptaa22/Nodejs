410093173d58e5725ddaf6d16b45ac97d0705b76
rohitGupta2022 <104063767+rohitGupta2022@users.noreply.github.com>
2022-11-10 18:31:34 +0530

    Basic Routing for manual urlChange

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http')
+const url = require('url')
 
 ////////////////////////////////
 // FILES
@@ -36,7 +37,15 @@ const http = require('http')
 // SERVER
 
 const server = http.createServer((req, res) => {
-    res.end('hello From the server!')
+    console.log(req.url);
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW!')
+    }
+    else if (pathName === '/product') {
+        res.end('This is the PRODUCT!')
+    }
+
 })
 
 server.listen(8000, '127.0.0.1', () => {
